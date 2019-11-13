22a125e89466b9eb96e6a10459b0c9315c02b188
Bispo <daniel@bisponet.com>
2019-11-13 13:05:13 -0300

    Adding a Controller

---
+++ controller/controller.go
@@ -0,0 +1,16 @@
+package controller
+
+import(
+	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func Hello(w http.ResponseWriter. r *http.Request) {
+	log.Info("hello func hit,responding.")
+
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, "Oh, hi :)")
+}
